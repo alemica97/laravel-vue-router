d1744c20f8059d233b60a2de883f8535f2a6ffa3
alemica97 <alessandro.micalizzi08@gmail.com>
2022-05-03 16:39:05 +0200

    correzione fatta a lezione

--- database/seeds/PostSeeder.php
+++ database/seeds/PostSeeder.php
@@ -20,7 +20,7 @@ class PostSeeder extends Seeder
             $post->title = ucfirst($faker->words(8, true));
             $post->slug = Str::slug( $post->title, '-' );
             $post->content = $faker->paragraphs(10, true);
-            $post->published_at = $faker->randomElement( [null, $faker->dateTime] );
+            $post->published_at = $faker->optional()->dateTime//$faker->randomElement( [null, $faker->dateTime] );
 
             $post->save();
         }
